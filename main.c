2d7db4a5760b710c1977dfca396c62221b3bece2
carruca <tomtomsierra@gmail.com>
2021-03-11 02:05:19 +0100

    Add get_delim funtion and try tok implementation

--- main.c
+++ main.c
@@ -15,6 +15,30 @@ char	*read_command_line()
 	return (line);
 }
 
+char	*get_delim(char *line)
+{
+	if (ft_strchr(line, '\"'))
+		return ("\"");
+	else if (ft_strchr(line, '\''))
+		return ("\'");
+	else if (ft_strchr(line, ' '))
+		return (" ");
+	return (" ");
+}
+
+void	lexer(char **line)
+{
+	char	*tok;
+
+	tok = NULL;
+	tok = ft_strtok(*line, get_delim(*line));
+	while (tok != NULL)
+	{
+		printf("%s\n", tok);
+		tok = ft_strtok(NULL, get_delim(*line));
+	}
+}
+
 void	parser()
 {
 	//Token parsing and expansions
@@ -35,6 +59,7 @@ void	read_eval_print_loop()
 		print_prompt();
 		cmd_line = read_command_line();
 		printf("%s\n", cmd_line);
+		lexer(&cmd_line);
 		if (!ft_strcmp(cmd_line, "exit"))
 		{
 			free(cmd_line);
