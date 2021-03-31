91faa2e62f95f678f36265c9f068f4fab9dcbf55
carruca <tomtomsierra@gmail.com>
2021-03-31 12:44:54 +0200

    Implement more token errors

--- parser.c
+++ parser.c
@@ -18,8 +18,8 @@ void	print_redir(void *redir)
 	t_redir	*tmp;
 
 	tmp = redir;
-	printf("redir file = %s\n", tmp->file);
-	printf("redir type = %d\n", tmp->type);
+	printf("-------redir file = %s\n", tmp->file);
+	printf("-------redir type = %d\n", tmp->type);
 }
 
 void	print_arg(void *arg)
@@ -27,7 +27,27 @@ void	print_arg(void *arg)
 	char *tmp;
 
 	tmp = arg;
-	printf("arg = %s\n", tmp);
+	printf("----arg = %s\n", tmp);
+}
+
+void	print_command(void *command)
+{
+	t_cmd	*tmp;
+
+	tmp = command;
+	printf("--Command:\n");
+	ft_lstiter(tmp->args_lst, print_arg);
+	ft_lstiter(tmp->redir_lst, print_redir);
+	printf("----pipe = %d\n", tmp->pipe);
+}
+
+void	print_pipeline(void *pipeline)
+{
+	t_pip	*tmp;
+
+	tmp = pipeline;
+	printf("Pipeline:\n");
+	ft_lstiter(tmp->cmd_lst, print_command);
 }
 
 void	del_current_token(t_list **tkn_lst)
@@ -82,18 +102,70 @@ t_redir	*parse_redirection(t_list **tkn_lst, int type)
 	return (redir);
 }
 
+void	parse_args(t_list **tkn_lst, t_list **args_lst)
+{
+	t_token	*atkn;
+
+	atkn = (*tkn_lst)->content;
+	while (*tkn_lst && atkn->identifier == WORD)
+	{
+		atkn = (*tkn_lst)->content;
+		ft_lstadd_back(args_lst,
+				ft_lstnew(ft_strdup(atkn->token)));
+		del_current_token(tkn_lst);
+	}
+}
+
+void	parse_redirection(t_list **tkn_lst, t_list **redir_lst, int id)
+{
+	t_token	*atkn;
+
+	ft_lstadd_back(redir_lst,
+			ft_lstnew(parse_redirection(tkn_lst, id)));
+	atkn = (*tkn_lst)->content;
+	(while *tkn_)
+}
+
+void	parse_simple_command(t_list **tkn_lst, t_cmd **cmd)
+{
+	t_token	*atkn;
+	t_cmd	*acmd;
+	int		id;
+
+	acmd = *cmd;
+	while (*tkn_lst)
+	{
+		atkn = NULL;
+		atkn = (*tkn_lst)->content;
+		id = atkn->identifier;
+		if (id == WORD)
+			parse_args(tkn_lst, &acmd->args_lst);
+		else if (id == GREAT || id == DGREAT || id == LESS)
+			parse_redirection(tkn_lst, &acmd->redir_lst, id);
+		else if (id == PIPE)
+		{
+			acmd->pipe = 1;
+			del_current_token(tkn_lst);
+			break ;
+		}
+
+	}
+}
+
 t_cmd	*new_simple_command(t_list **tkn_lst)
 {
 	t_token	*atkn;
 	t_cmd	*cmd;
+	char	stop_word;
 
 	cmd = ft_calloc(1, sizeof(t_cmd));
 	if (!cmd)
 		return (NULL);
+	stop_word = 0;
 	while (*tkn_lst)
 	{
 		atkn = (*tkn_lst)->content;
-		if (atkn->identifier == WORD)
+		if (atkn->identifier == WORD && !stop_word)
 			ft_lstadd_back(&cmd->args_lst,
 					ft_lstnew(ft_strdup(atkn->token)));
 		else if (atkn->identifier == PIPE)
@@ -104,15 +176,17 @@ t_cmd	*new_simple_command(t_list **tkn_lst)
 		}
 		else if (atkn->identifier == GREAT || atkn->identifier == DGREAT 
 				|| atkn->identifier == LESS)
+		{
 			ft_lstadd_back(&cmd->redir_lst,
 					ft_lstnew(parse_redirection(tkn_lst, atkn->identifier)));
-		else
+			stop_word = 0x01;
+		}
+		else if (atkn->identifier == SCOLON)
 			break ;
 		del_current_token(tkn_lst);
 	}
-	ft_lstiter(cmd->args_lst, print_arg);
-	ft_lstiter(cmd->redir_lst, print_redir);
-	printf("pipe = %d\n", cmd->pipe);
+//	ft_lstiter(cmd->args_lst, print_arg);
+//	ft_lstiter(cmd->redir_lst, print_redir);
 	return (cmd);
 }
 
@@ -131,8 +205,8 @@ t_list	*parse_command(t_list **tkn_lst)
 			break ;
 		}
 		ft_lstadd_back(&cmd_lst, ft_lstnew(new_simple_command(tkn_lst)));	
-		printf("Command found\n");
 	}
+	//ft_lstiter(cmd_lst, print_command);
 	return (cmd_lst);
 }
 
@@ -167,11 +241,9 @@ t_list	*parse_pipeline(t_list **tkn_lst)
 		{
 			ft_lstadd_back(&pipeline_lst,
 					ft_lstnew(new_pipeline(tkn_lst)));
-			printf("Pipeline found\n");
 		}
 		else
 			print_syntax_error(atkn->token, tkn_lst);
-		ft_lstclear(&pipeline_lst, free_pipeline);
 	}
 	return (pipeline_lst);
 }
@@ -184,5 +256,9 @@ t_list	*parser(char *input)
 	tkn_lst = tokenizer(input);
 //	ft_lstiter(tkn_lst, &print_token);
 	pipeline_lst = parse_pipeline(&tkn_lst);
+	if (!pipeline_lst)
+		return (NULL);
+	ft_lstiter(pipeline_lst, print_pipeline);
+	ft_lstclear(&pipeline_lst, free_pipeline);
 	return (pipeline_lst);
 }
