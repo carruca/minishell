a94e5b642d310dabe164dd47ebbd7ab0fa42dd87
Tomas Sierra paradinas <tsierra-@c1r11s4.42madrid.com>
2021-03-31 16:52:05 +0200

    Implement new syntax error for pipe and redir

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: tsierra- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 16:46:50 by tsierra-          #+#    #+#             */
-/*   Updated: 2021/03/29 18:33:42 by tsierra-         ###   ########.fr       */
+/*   Updated: 2021/03/31 16:50:52 by tsierra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,8 +73,20 @@ void	free_command(void *cmd)
 	t_cmd	*tmp;
 
 	tmp = cmd;
-	ft_lstclear(&tmp->args_lst, free);
-	ft_lstclear(&tmp->redir_lst, free_redir);
+	if (tmp->args_lst)
+	{
+		if (tmp->args_lst->content)
+			ft_lstclear(&tmp->args_lst, free);
+		else
+			free(tmp->args_lst);
+	}
+	if (tmp->redir_lst)
+	{
+		if (tmp->redir_lst->content)
+			ft_lstclear(&tmp->redir_lst, free_redir);
+		else
+			free(tmp->redir_lst);
+	}
 	free(tmp);
 }
 
@@ -83,17 +95,40 @@ void	free_pipeline(void *pipeline)
 	t_pip	*tmp;
 
 	tmp = pipeline;
-	ft_lstclear(&tmp->cmd_lst, free_command);
+	if (tmp->cmd_lst)
+	{
+		if (tmp->cmd_lst->content)
+			ft_lstclear(&tmp->cmd_lst, free_command);
+		else
+			free(tmp->cmd_lst);
+	}
 	free(tmp);
 }
 
-t_redir	*parse_redirection(t_list **tkn_lst, int type)
+void	print_syntax_error(char *token, t_list **tkn_lst)
+{
+	printf("minishell: syntax error near unexpected token `%s'\n", token);
+	if (tkn_lst)
+		ft_lstclear(tkn_lst, free_token);
+}
+
+t_redir	*new_redirection(t_list **tkn_lst, int type)
 {
 	t_redir	*redir;
 	t_token	*atkn;
 
 	del_current_token(tkn_lst);
+	if (!(*tkn_lst))
+	{
+		print_syntax_error("newline", tkn_lst);
+		return (NULL);
+	}
 	atkn = (*tkn_lst)->content;
+	if (atkn->identifier != WORD)
+	{
+		print_syntax_error(atkn->token, tkn_lst);
+		return (NULL);
+	}
 	redir = malloc(sizeof(t_redir));
 	if (!redir)
 		return (NULL);
@@ -109,21 +144,47 @@ void	parse_args(t_list **tkn_lst, t_list **args_lst)
 	atkn = (*tkn_lst)->content;
 	while (*tkn_lst && atkn->identifier == WORD)
 	{
-		atkn = (*tkn_lst)->content;
 		ft_lstadd_back(args_lst,
 				ft_lstnew(ft_strdup(atkn->token)));
 		del_current_token(tkn_lst);
+		if (*tkn_lst)
+			atkn = (*tkn_lst)->content;
 	}
 }
 
 void	parse_redirection(t_list **tkn_lst, t_list **redir_lst, int id)
 {
 	t_token	*atkn;
+	t_redir	*aredir;
 
-	ft_lstadd_back(redir_lst,
-			ft_lstnew(parse_redirection(tkn_lst, id)));
+	aredir = new_redirection(tkn_lst, id);
+	if (!aredir)
+		return ;
+	ft_lstadd_back(redir_lst, ft_lstnew(aredir));
 	atkn = (*tkn_lst)->content;
-	(while *tkn_)
+	printf("Entra\n");
+	while (*tkn_lst && atkn->identifier == WORD)
+	{
+		del_current_token(tkn_lst);
+		if (*tkn_lst)
+			atkn = (*tkn_lst)->content;
+	}
+}
+
+void	parse_pipe(t_list **tkn_lst, int *pipe)
+{
+	t_token	*atkn;
+
+	*pipe = 1;
+	del_current_token(tkn_lst);
+	if (!(*tkn_lst))
+	{
+		print_syntax_error("newline", tkn_lst);
+		return ;
+	}
+	atkn = (*tkn_lst)->content;
+	if (atkn->identifier == PIPE)
+		print_syntax_error(atkn->token, tkn_lst);
 }
 
 void	parse_simple_command(t_list **tkn_lst, t_cmd **cmd)
@@ -133,9 +194,9 @@ void	parse_simple_command(t_list **tkn_lst, t_cmd **cmd)
 	int		id;
 
 	acmd = *cmd;
+	atkn = NULL;
 	while (*tkn_lst)
 	{
-		atkn = NULL;
 		atkn = (*tkn_lst)->content;
 		id = atkn->identifier;
 		if (id == WORD)
@@ -144,11 +205,16 @@ void	parse_simple_command(t_list **tkn_lst, t_cmd **cmd)
 			parse_redirection(tkn_lst, &acmd->redir_lst, id);
 		else if (id == PIPE)
 		{
-			acmd->pipe = 1;
-			del_current_token(tkn_lst);
+			parse_pipe(tkn_lst, &acmd->pipe);
+			if (!(*tkn_lst))
+			{
+				free_command(acmd);
+				return ;
+			}
 			break ;
 		}
-
+		else if (id == SCOLON)
+			break ;
 	}
 }
 
@@ -162,6 +228,9 @@ t_cmd	*new_simple_command(t_list **tkn_lst)
 	if (!cmd)
 		return (NULL);
 	stop_word = 0;
+/*	parse_simple_command(tkn_lst, &cmd);
+	if (!cmd)
+		return (NULL);*/
 	while (*tkn_lst)
 	{
 		atkn = (*tkn_lst)->content;
@@ -170,32 +239,42 @@ t_cmd	*new_simple_command(t_list **tkn_lst)
 					ft_lstnew(ft_strdup(atkn->token)));
 		else if (atkn->identifier == PIPE)
 		{
-			cmd->pipe = 1;
-			del_current_token(tkn_lst);
+			parse_pipe(tkn_lst, &cmd->pipe);
+			if (!(*tkn_lst))
+			{
+				free_command(cmd);
+				return (NULL);
+			}
 			break ;
 		}
 		else if (atkn->identifier == GREAT || atkn->identifier == DGREAT 
 				|| atkn->identifier == LESS)
 		{
 			ft_lstadd_back(&cmd->redir_lst,
-					ft_lstnew(parse_redirection(tkn_lst, atkn->identifier)));
+					ft_lstnew(new_redirection(tkn_lst, atkn->identifier)));
+			if (!cmd->redir_lst->content)
+			{
+				free_command(cmd);
+				return (NULL);
+			}
 			stop_word = 0x01;
 		}
 		else if (atkn->identifier == SCOLON)
 			break ;
-		del_current_token(tkn_lst);
+		if (*tkn_lst)
+			del_current_token(tkn_lst);
 	}
-//	ft_lstiter(cmd->args_lst, print_arg);
-//	ft_lstiter(cmd->redir_lst, print_redir);
 	return (cmd);
 }
 
 t_list	*parse_command(t_list **tkn_lst)
 {
 	t_list	*cmd_lst;
+	t_cmd	*acmd;
 	t_token	*atkn;
 
 	cmd_lst = NULL;
+	acmd = NULL;
 	while (*tkn_lst)
 	{
 		atkn = (*tkn_lst)->content;
@@ -204,9 +283,14 @@ t_list	*parse_command(t_list **tkn_lst)
 			del_current_token(tkn_lst);
 			break ;
 		}
-		ft_lstadd_back(&cmd_lst, ft_lstnew(new_simple_command(tkn_lst)));	
+		acmd = new_simple_command(tkn_lst);
+		if (!acmd)
+		{
+			ft_lstclear(&cmd_lst, free_command);
+			return (NULL);
+		}
+		ft_lstadd_back(&cmd_lst, ft_lstnew(acmd));
 	}
-	//ft_lstiter(cmd_lst, print_command);
 	return (cmd_lst);
 }
 
@@ -218,29 +302,35 @@ t_pip	*new_pipeline(t_list **tkn_lst)
 	if (!new_pipeline)
 		return (NULL);
 	new_pipeline->cmd_lst = parse_command(tkn_lst);
+	if (!new_pipeline->cmd_lst)
+	{
+		free_pipeline(new_pipeline);
+		return (NULL);
+	}
 	return (new_pipeline);
-}
-
-void	print_syntax_error(char *token, t_list **tkn_lst)
-{
-	printf("minishell: syntax error near unexpected token `%s'\n", token);
-	ft_lstclear(tkn_lst, free_token);
 }
 
 t_list	*parse_pipeline(t_list **tkn_lst)
 {
 	t_token	*atkn;
 	t_list	*pipeline_lst;
+	t_pip	*apipeline;
 
 	pipeline_lst = NULL;
 	atkn = NULL;
+	apipeline = NULL;
 	while (*tkn_lst)
 	{
 		atkn = (*tkn_lst)->content;
 		if (atkn->identifier != SCOLON && atkn->identifier != PIPE)
 		{
-			ft_lstadd_back(&pipeline_lst,
-					ft_lstnew(new_pipeline(tkn_lst)));
+			apipeline = new_pipeline(tkn_lst);
+			if (!apipeline)
+			{
+				ft_lstclear(&pipeline_lst, free_pipeline);
+				return (NULL);
+			}
+			ft_lstadd_back(&pipeline_lst, ft_lstnew(apipeline));
 		}
 		else
 			print_syntax_error(atkn->token, tkn_lst);
