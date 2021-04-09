d326da0857af67c747269d7713fae60b25f3f0ac
carruca <tomtomsierra@gmail.com>
2021-04-09 02:40:57 +0200

    Change function name and generic strjoin_bwtchar

--- executer.c
+++ executer.c
@@ -54,7 +54,7 @@ void	print_argv(char **argv)
 	}
 }
 
-int		search_dir(char *path, char *name)
+int		search_directory(char *path, char *name)
 {
 	DIR				*dirp;
 	struct dirent	*dp;
@@ -77,54 +77,52 @@ int		search_dir(char *path, char *name)
 	return (0);
 }
 
-char	*get_path(char *dir_path, char *name)
+char	*ft_strjoin_btwchar(char *s1, char *s2, char c)
 {
-	char	*path;
-	int		name_len;
-	int		dir_len;
+	char	*dst;
+	int		s1_len;
+	int		s2_len;
 
-	dir_len = ft_strlen(dir_path);
-	name_len = ft_strlen(name);
-	path = malloc(sizeof(char) * (dir_len + name_len + 2));
-	if (!path)
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	dst = malloc(sizeof(char) * (s1_len + s2_len + 2));
+	if (!dst)
 		return (NULL);
-	ft_strlcpy(path, dir_path, dir_len + 1);
-	path[dir_len] = '/';
-	ft_strlcpy(path + dir_len + 1, name, name_len + 1);
-	path[dir_len + name_len + 1] = '\0';
-	return (path);
+	ft_strlcpy(dst, s1, s1_len + 1);
+	dst[s1_len] = c;
+	ft_strlcpy(dst + s1_len + 1, s2, s2_len + 1);
+	dst[s1_len + s2_len + 1] = '\0';
+	return (dst);
 }
 
-char	*find_path(char *name)
+char	*get_exe_path(char *name)
 {
-	char	*path_dir;
-	char	*path_env;
-	char	*path;
+	char	*env_path;
+	char	*dir_path;
+	char	*exe_path;
 
-	if (!ft_strchr(name, '/'))
-	{
-		path_env = ft_strdup(getenv("PATH"));
-		path_dir = ft_strtok(path_env, ":");
-		while (path_dir != NULL)
-		{
-			if (search_dir(path_dir, name))
-			{
-				path = get_path(path_dir, name);
-				free(path_env);
-				return (path);
-			}
-			path_dir = ft_strtok(NULL, ":");
-		}
-		free(path_env);
-	}
-	else
+	if (ft_strchr(name, '/'))
 		return (name);
+	env_path = ft_strdup(getenv("PATH"));
+	dir_path = ft_strtok(env_path, ":");
+	while (dir_path != NULL)
+	{
+		if (search_directory(dir_path, name))
+		{
+			exe_path = ft_strjoin_btwchar(dir_path, name, '/');
+			free(env_path);
+			return (exe_path);
+		}
+		dir_path = ft_strtok(NULL, ":");
+	}
+	free(env_path);
 	return (NULL);
 }
 
 void	executer_command(t_cmd *cmd)
 {
 	int		status;
+	int		ret;
 	pid_t	child_pid;
 	char	**argv;
 	char	*path;
@@ -135,8 +133,12 @@ void	executer_command(t_cmd *cmd)
 	else if (child_pid == 0)
 	{
 		argv = lst_to_argv(cmd->args_lst);
-		path = find_path(argv[0]);
-		execve(path, argv, environ);
+		path = get_exe_path(argv[0]);
+		if (!path)
+			exit(EXIT_FAILURE);
+		if ((ret = execve(path, argv, environ)) < 0)
+			printf("minishell: %s: %s\n", argv[0], strerror(errno));
+		printf("ret = %i\n", ret);
 		free(path);
 		ft_free_tab(argv);
 		exit(0);
@@ -146,21 +148,18 @@ void	executer_command(t_cmd *cmd)
 
 void	executer_pipeline(t_pip *pipeline)
 {
-	t_list	*head;
+	t_list	*head_lst;
 	t_cmd	*acmd;
 
-	head = pipeline->cmd_lst;
+	head_lst = pipeline->cmd_lst;
 	while (pipeline->cmd_lst)
 	{
 		acmd = pipeline->cmd_lst->content;
 		if (acmd)
-		{
-		//	printf("cmd\n");
 			executer_command(acmd);
-		}
 		pipeline->cmd_lst = pipeline->cmd_lst->next;
 	}
-	ft_lstclear(&head, free_command);
+	ft_lstclear(&head_lst, free_command);
 }
 
 void	executer(t_list *pipeline_lst)
@@ -173,10 +172,7 @@ void	executer(t_list *pipeline_lst)
 	{
 		apipeline = pipeline_lst->content;
 		if (apipeline)
-		{
-		//	printf("pipeline\n");
 			executer_pipeline(apipeline);
-		}
 		pipeline_lst = pipeline_lst->next;
 	}
 	ft_lstclear(&head, free_pipeline);
