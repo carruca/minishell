c3aef74c02ebc35fc09a89846e1684adf2735f14
carruca <tomtomsierra@gmail.com>
2021-05-19 15:45:08 +0200

    Change set_env function, unfinished

--- builtins/cd.c
+++ builtins/cd.c
@@ -12,6 +12,76 @@
 
 #include "../minishell.h"
 
+int	cmp_value_var(t_var *var, char *value)
+{
+	if (!ft_strcmp(var->value, value))
+		return (1);
+	return (0);
+}
+
+t_lista	*ft_lstfind_double(t_lista *lst, void *content, int (*cmp)())
+{
+	if (lst && cmp)
+	{
+		while (lst)
+		{
+			if (!cmp(content, lst->content))
+				return (lst);
+			lst = lst->next;
+		}
+	}
+	return (NULL);
+}
+
+t_lista	*ft_lstnew_double(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = (void *)content;
+	new->next = NULL;
+	return (new);
+}
+
+t_lista	*ft_lstlast_double(t_lista *lst)
+{
+	if (lst)
+		while (lst->next)
+			lst = lst->next;
+	return (lst);
+}
+
+
+int	set_env_2(t_lista *lst, char *name, char *value, int overwrite)
+{
+	t_lista	
+	t_var	*var;
+	char	*ptr;
+
+	var = ft_lstfind_double(lst, &cmp_value_var);
+	if (!var)
+	{
+		lst = ft_lstlast_double(lst);
+		lst->next = ft_lstnew()
+	}
+	
+/*	while (lst)
+	{
+		var = lst->content;
+		if (!ft_strcmp(name, var->name) && overwrite)
+		{
+			ptr	= var->value;
+			var->value = ft_strdup(value);
+			free(ptr);
+			return (1);
+		}
+		lst = lst->next;
+	}*/
+	return (0);
+}
+
 int	builtin_cd(t_shell *sh, int	argc, char **argv)
 {
 	int		ret;
@@ -33,13 +103,13 @@ int	builtin_cd(t_shell *sh, int	argc, char **argv)
 		oldpwd = find_node("PWD", sh->_env.env_lst);
 		if (oldpwd)
 		{
-			set_env(sh->_env.env_lst, "PWD",
-					getcwd(pwd, sizeof(char) * 2048));
-			if (find_node("OLDPWD", sh->_env.env_lst) != NULL)
+			set_env_2(sh->_env.env_lst, "PWD",
+					getcwd(pwd, sizeof(char) * 2048), 1);
+		/*	if (find_node("OLDPWD", sh->_env.env_lst) != NULL)
 			{
-				oldpwd = ft_strdup(oldpwd);
+			//	oldpwd = ft_strdup(oldpwd);
 				set_env(sh->_env.env_lst, "OLDPWD", oldpwd);
-			}
+			}*/
 		}
 		sh->status = 0;
 	}
